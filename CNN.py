4b9450ebcb43c32721330540ddfbba5104ad7cc1
LukeVidalis <loukaslks@gmail.com>
2019-07-07 16:54:36 +0100

    Setting up test layers

--- CNN.py
+++ CNN.py
@@ -1,11 +1,12 @@
-import keras
-from keras.models import Model, Sequential
-from keras.layers import Input, concatenate, Conv2D, MaxPooling2D, Dropout, MaxPooling2D, Activation
-from keras.optimizers import Adam
-from keras.callbacks import ModelCheckpoint
-from keras.utils import plot_model
-from keras import backend as K
-import os
+from keras.models import Sequential
+from keras.layers import Conv2D, Dropout, MaxPooling2D, Activation
+from keras.layers.convolutional_recurrent import ConvLSTM2D
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import Conv3D
+
+import numpy as np
+import pylab as plt
+
 
 img_width = 128
 img_height = 128
@@ -22,6 +23,21 @@ model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 
 
+seq = Sequential()
+seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3), input_shape=(None, 40, 40, 1), padding="same",
+                   return_sequences=True))
+seq.add(BatchNormalization())
 
+seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3), padding="same", return_sequences=True))
+seq.add(BatchNormalization())
+
+seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3), padding="same", return_sequences=True))
+seq.add(BatchNormalization())
+
+seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3), padding="same", return_sequences=True))
+seq.add(BatchNormalization())
+
+seq.add(Conv3D(filters=1, kernel_size=(3, 3, 3), activation="sigmoid", padding="same", data_format="channels_last"))
+seq.compile(loss="binary_crossentropy", optimizer="adadelta")
 
 
