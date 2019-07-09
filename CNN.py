4bfc6e041d6778850f3e60188ac1d5fe64dfd0ee
sgg <sentogaliana13@gmail.com>
2019-07-09 14:04:33 +0100

    Reverse Commit

--- CNN.py
+++ CNN.py
@@ -13,34 +13,50 @@ rgb = 3
 epochs_num = 100
 batch_size = 250
 
+
 def load_files():
     data = load_array("Acid_Plant")
     return data['x'], data['Y']
 
-
-def setup(x, Y):
-    seq = Sequential()
-    seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3), input_shape=(img_width, img_height, rgb), padding="same",
+def create_model():
+    model.add(ConvLSTM2D(filters=32, kernel_size=(3, 3), input_shape=(img_width, img_height, rgb), padding="same",
                        return_sequences=True))
-    seq.add(BatchNormalization())
+    model.add(BatchNormalization())
 
-    seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3), padding="same", return_sequences=True))
-    seq.add(BatchNormalization())
+    model.add(ConvLSTM2D(filters=32, kernel_size=(3, 3), padding="same", return_sequences=True))
+    model.add(BatchNormalization())
 
-    seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3), padding="same", return_sequences=True))
-    seq.add(BatchNormalization())
+    model.add(ConvLSTM2D(filters=32, kernel_size=(3, 3), padding="same", return_sequences=True))
+    model.add(BatchNormalization())
 
-    seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3), padding="same", return_sequences=True))
-    seq.add(BatchNormalization())
+    model.add(ConvLSTM2D(filters=32, kernel_size=(3, 3), padding="same", return_sequences=True))
+    model.add(BatchNormalization())
 
-    seq.add(Conv3D(filters=1, kernel_size=(3, 3, 3), activation="sigmoid", padding="same", data_format="channels_last"))
-    seq.compile(loss="binary_crossentropy", optimizer="adadelta")
+    model.add(Conv3D(filters=1, kernel_size=(3, 3, 3), activation="sigmoid", padding="same",
+                     data_format="channels_last"))
+    model.compile(loss="binary_crossentropy", optimizer="adadelta")
 
-    seq.fit(x=x, y=Y, batch_size=batch_size, epochs=epochs_num, verbose=1, callbacks=None, validation_split=0.2,
+    return model
+
+
+def save_model(model):
+    """ code here """
+
+
+def load_model():
+    """ code here """
+
+
+def train_model(model, x, Y):
+
+    model.fit(x=x, y=Y, batch_size=batch_size, epochs=epochs_num, verbose=1, callbacks=None, validation_split=0.2,
             validation_data=None, shuffle=True, class_weight=None, sample_weight=None, initial_epoch=0,
             steps_per_epoch=None, validation_steps=None, validation_freq=1)
+
+    return model
 
 
 if __name__ == "__main__":
     x, Y = load_files()
-    setup(x, Y)
+    model = create_model()
+    model = train_model(model, x, Y)
