ef494c9d8526f2f85a2f23acc753b5cc95c855a7
LukeVidalis <loukaslks@gmail.com>
2019-07-09 13:33:48 +0100

    Changed format of CNN class and added method to load data

--- CNN.py
+++ CNN.py
@@ -3,41 +3,44 @@ from keras.layers import Conv2D, Dropout, MaxPooling2D, Activation
 from keras.layers.convolutional_recurrent import ConvLSTM2D
 from keras.layers.normalization import BatchNormalization
 from keras.layers.convolutional import Conv3D
-
+from process_array import *
 import numpy as np
-import pylab as plt
 
-
+# Parameters
 img_width = 128
 img_height = 128
 rgb = 3
+epochs_num = 100
+batch_size = 250
 
-model = Sequential()
-
-model.add(Conv2D(64, (3, 3), padding="same", input_shape=(128, 128, 3)))
-model.add(Activation("relu"))
-
-model.add(Conv2D(32, (3, 3)))
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
+def load_files():
+    data = load_array("Acid_Plant")
+    return data['x'], data['Y']
 
 
-seq = Sequential()
-seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3), input_shape=(None, 40, 40, 1), padding="same",
-                   return_sequences=True))
-seq.add(BatchNormalization())
+def setup(x, Y):
+    seq = Sequential()
+    seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3), input_shape=(img_width, img_height, rgb), padding="same",
+                       return_sequences=True))
+    seq.add(BatchNormalization())
 
-seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3), padding="same", return_sequences=True))
-seq.add(BatchNormalization())
+    seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3), padding="same", return_sequences=True))
+    seq.add(BatchNormalization())
 
-seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3), padding="same", return_sequences=True))
-seq.add(BatchNormalization())
+    seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3), padding="same", return_sequences=True))
+    seq.add(BatchNormalization())
 
-seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3), padding="same", return_sequences=True))
-seq.add(BatchNormalization())
+    seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3), padding="same", return_sequences=True))
+    seq.add(BatchNormalization())
 
-seq.add(Conv3D(filters=1, kernel_size=(3, 3, 3), activation="sigmoid", padding="same", data_format="channels_last"))
-seq.compile(loss="binary_crossentropy", optimizer="adadelta")
+    seq.add(Conv3D(filters=1, kernel_size=(3, 3, 3), activation="sigmoid", padding="same", data_format="channels_last"))
+    seq.compile(loss="binary_crossentropy", optimizer="adadelta")
+
+    seq.fit(x=x, y=Y, batch_size=batch_size, epochs=epochs_num, verbose=1, callbacks=None, validation_split=0.2,
+            validation_data=None, shuffle=True, class_weight=None, sample_weight=None, initial_epoch=0,
+            steps_per_epoch=None, validation_steps=None, validation_freq=1)
 
 
+if __name__ == "__main__":
+    x, Y = load_files()
+    setup(x, Y)
