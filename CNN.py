d04bd8347fc8f9456a59a056ffcc750704026714
LukeVidalis <loukaslks@gmail.com>
2019-07-09 14:36:47 +0100

    Implemented Save model / settings class

--- CNN.py
+++ CNN.py
@@ -4,7 +4,11 @@ from keras.layers.convolutional_recurrent import ConvLSTM2D
 from keras.layers.normalization import BatchNormalization
 from keras.layers.convolutional import Conv3D
 from process_array import *
-import numpy as np
+import os
+from settings import *
+
+# Paths
+json_file = os.path.join(WEIGHTS_DIR, 'CNN_model.json')
 
 # Parameters
 img_width = 128
@@ -18,9 +22,10 @@ def load_files():
     data = load_array("Acid_Plant")
     return data['x'], data['Y']
 
+
 def create_model():
     model.add(ConvLSTM2D(filters=32, kernel_size=(3, 3), input_shape=(img_width, img_height, rgb), padding="same",
-                       return_sequences=True))
+                         return_sequences=True))
     model.add(BatchNormalization())
 
     model.add(ConvLSTM2D(filters=32, kernel_size=(3, 3), padding="same", return_sequences=True))
@@ -40,7 +45,10 @@ def create_model():
 
 
 def save_model(model):
-    """ code here """
+    # TODO add weights file
+    json_string = model.to_json()
+    with open(json_file, "w") as f:
+        f.write(json_string)
 
 
 def load_model():
@@ -50,8 +58,8 @@ def load_model():
 def train_model(model, x, Y):
 
     model.fit(x=x, y=Y, batch_size=batch_size, epochs=epochs_num, verbose=1, callbacks=None, validation_split=0.2,
-            validation_data=None, shuffle=True, class_weight=None, sample_weight=None, initial_epoch=0,
-            steps_per_epoch=None, validation_steps=None, validation_freq=1)
+              validation_data=None, shuffle=True, class_weight=None, sample_weight=None, initial_epoch=0,
+              steps_per_epoch=None, validation_steps=None, validation_freq=1)
 
     return model
 
---
+++ settings.py
@@ -0,0 +1,9 @@
+
+# Directory of the frames
+DATA_DIR = './Frames/'
+
+# Where model weights and config will be saved
+WEIGHTS_DIR = './Model/'
+
+# Where results (prediction plots and evaluation file) will be saved.
+RESULTS_SAVE_DIR = './Results/'
