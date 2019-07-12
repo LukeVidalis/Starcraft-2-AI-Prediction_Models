64b0aec597554286f2252832807b657dc4ad6115
sgg <sentogaliana13@gmail.com>
2019-07-12 18:12:04 +0100

    Testing class started

--- evaluation.py
+++ evaluation.py
@@ -1,8 +1,15 @@
 from keras.models import load_model
+from settings import *
+import os
 
+json_file_model = os.path.join(WEIGHTS_DIR, 'CNN_model.json')
+json_file_weights = os.path.join(WEIGHTS_DIR, 'model2.h5')
+seq_model = None
+model_weights = None
 
 def load():
-    print()
+    seq_model = load_model(json_file_model)
+
 
 
 if __name__ == "__main__":
