3f333fb6102cc5694ece4ce98544ed58186880e4
LukeVidalis <loukaslks@gmail.com>
2019-07-12 18:11:08 +0100

    Implemented Load Function

--- evaluation.py
+++ evaluation.py
@@ -1,9 +1,20 @@
 from keras.models import load_model
+import os
+from settings import *
 
 
-def load():
-    print()
+def load(filename):
+    json_file = os.path.join(WEIGHTS_DIR, filename)
+    weight_file = os.path.join(WEIGHTS_DIR, "Model1.h5")
+    mod =   load_model(json_file)
+    mod.load_weights(weight_file)
+    return mod
+
+def evaluate(model):
+    
+
 
 
 if __name__ == "__main__":
-    load()
+    model = load("CNN_model.json")
+    evaluate(model)
