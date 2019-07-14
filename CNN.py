651f493b2795fb93bb591cab0e3c55b4a2228d50
LukeVidalis <loukaslks@gmail.com>
2019-07-14 20:08:31 +0100

    Cleaned up code and set up a better system for changing configurations

--- CNN.py
+++ CNN.py
@@ -1,28 +1,15 @@
-from keras.models import Sequential, load_model
+from keras.models import Sequential
 from keras.layers import Conv2D, Activation, Dropout
 from keras.layers.normalization import BatchNormalization
-from process_array import *
-import os
-from settings import *
-from numba import vectorize
-
-# Paths
-json_file = os.path.join(WEIGHTS_DIR, 'CNN_model.json')
 
 # Parameters
 img_width = 128
 img_height = 128
 rgb = 3
-epochs_num = 100
-batch_size = 1
-
-
-def load_files():
-    data = load_array("Acid_Plant10.npz")
-    return data['x'], data['Y']
 
 
 def create_model():
+    print("Creating Model")
     model = Sequential()
 
     model.add(Conv2D(filters=32, kernel_size=(7, 7), input_shape=(img_width, img_height, rgb), padding="same"))
@@ -54,20 +41,6 @@ def create_model():
     model.add(BatchNormalization())
     model.add(Dropout(0.25))
 
-    # model.add(Flatten())
-    # model.add(Dense(512))
-    # model.add(Activation("sigmoid"))
-    # model.add(BatchNormalization())
-    # model.add(Dropout(0.5))
-
-
-    #
-    # seq_model.add(Conv2D(filters=32, kernel_size=(3, 3), padding="same"))
-    # seq_model.add(BatchNormalization())
-    #
-    # seq_model.add(Conv2D(filters=32, kernel_size=(3, 3), padding="same"))
-    # seq_model.add(BatchNormalization())
-
     model.add(Conv2D(filters=3, kernel_size=(3, 3), activation="sigmoid", padding="same"))
 
     model.compile(loss="binary_crossentropy", optimizer="adadelta")
@@ -75,49 +48,5 @@ def create_model():
     return model
 
 
-def save_model(model):
-    json_string = seq_model.to_json()
-    with open(json_file, "w") as f:
-        f.write(json_string)
-    model.save_weights("model.h5")
 
 
-def get_model():
-    return load_model(json_file)
-
-
-def train_model(model, x, Y):
-
-    hst = model.fit(x=x, y=Y, batch_size=batch_size, epochs=epochs_num, verbose=2, callbacks=None,
-                    validation_split=0.2, validation_data=None, shuffle=True, class_weight=None,
-                    sample_weight=None, initial_epoch=0, steps_per_epoch=None, validation_steps=None)
-
-    return hst, model
-
-    # x: Input
-    # y: Output
-    # batch_size: number of samples per gradient update
-    # epochs: number of epochs to train the model
-    # verbose: Verbosity mode (0, 1 OR 2)
-    # callbacks: callbacks to apply during training TODO: Look into callbacks
-    # validation_split: Fraction of the training data to be used as validation data
-    # validation_data: Data to perform validation on
-    # shuffle:  shuffle the training data before each epoch
-    # class_weight: dictionary for adding weight to different classes
-    # sample_weight: array of sample weights
-    # initial_epoch: epoch at which to start training. Useful for resuming a previous training run.
-    # steps_per_epoch: total number of steps (batches of samples) before declaring one epoch finished and
-    # starting the next epoch.
-    # validation_steps: if steps_per_epoch != None, total number of steps to validate before stopping
-    # validation_freq: run validation every x epochs. ( if validation_data != None)
-
-
-if __name__ == "__main__":
-    print("Getting Data")
-    x, Y = load_files()
-    print("\nCreating Model")
-    seq_model = create_model()
-    print("\nTraining Model")
-    history, seq_model = train_model(seq_model, x, Y)
-    print("\nSaving Model")
-    save_model(seq_model)
--- train.py
+++ train.py
@@ -1,15 +1,14 @@
-from keras.models import Sequential, load_model
-from keras.layers import Conv2D
-from keras.layers.normalization import BatchNormalization
-from process_array import *
 import os
+import time
+import datetime
+from keras.models import load_model
+from process_array import *
 from settings import *
+from CNN import create_model
 
-
-# Paths
-json_file = os.path.join(WEIGHTS_DIR, 'CNN_model.json')
 
 # Parameters
+model_id = 7
 img_width = 128
 img_height = 128
 rgb = 3
@@ -17,32 +16,22 @@ epochs_num = 100
 batch_size = 1
 
 
+# Paths
+json_file = os.path.join(WEIGHTS_DIR, 'CNN_model_'+str(model_id)+'.json')
+
+
 def load_files():
+    print("Getting Data")
     data = load_array("Acid_Plant10.npz")
     return data['x'], data['Y']
 
 
-def create_model():
-    model = Sequential()
-
-    model.add(Conv2D(filters=32, kernel_size=(3, 3), input_shape=(img_width, img_height, rgb), padding="same"))
-    model.add(BatchNormalization())
-
-    model.add(Conv2D(filters=64, kernel_size=(3, 3), padding="same"))
-    model.add(BatchNormalization())
-
-    model.add(Conv2D(filters=3, kernel_size=(3, 3), activation="sigmoid", padding="same"))
-
-    model.compile(loss="binary_crossentropy", optimizer="adadelta")
-
-    return model
-
-
 def save_model(model):
+    print("Saving Model")
     json_string = seq_model.to_json()
     with open(json_file, "w") as f:
         f.write(json_string)
-    model.save_weights("model.h5")
+    model.save_weights("model"+str(model_id)+".h5")
 
 
 def get_model():
@@ -50,11 +39,14 @@ def get_model():
 
 
 def train_model(model, x, Y):
-
+    print("Training Model")
+    print("Epochs: "+str(epochs_num)+"\nBatch Size: "+str(batch_size))
+    start = time.time()
     hst = model.fit(x=x, y=Y, batch_size=batch_size, epochs=epochs_num, verbose=2, callbacks=None,
                     validation_split=0.2, validation_data=None, shuffle=True, class_weight=None,
                     sample_weight=None, initial_epoch=0, steps_per_epoch=None, validation_steps=None)
-
+    end = time.time()
+    print("Time Elapsed: "+str(end-start))
     return hst, model
 
     # x: Input
@@ -76,11 +68,7 @@ def train_model(model, x, Y):
 
 
 if __name__ == "__main__":
-    print("Getting Data")
     x, Y = load_files()
-    print("\nCreating Model")
     seq_model = create_model()
-    print("\nTraining Model")
     history, seq_model = train_model(seq_model, x, Y)
-    print("\nSaving Model")
     save_model(seq_model)
